d40fbdfe7ad9adcd6c24c7092583df142e5810a2
Ulrike Fischer <fischer@troubleshooting-tex.de>
2022-03-07 20:12:02 +0100

    update testfiles to new latex

--- resave.bat
+++ resave.bat
@@ -1,3 +1,29 @@
 
 rem */\(1+[a-z\-]\)\.\(2+[a-z0-9]\)\.*
 rem l3build save -e\2 \1
+
+
+l3build save -eluatex zref-abspos-issue1
+l3build save -epdftex zref-abspos-issue1
+l3build save -exetex  zref-abspos-issue1
+l3build save -exetex  zref-counter-issue5
+l3build save -eluatex zref-pageattr
+l3build save -epdftex zref-pageattr
+l3build save -exetex zref-pageattr
+l3build save -eluatex zref-test-abspos
+l3build save -epdftex zref-test-abspos
+l3build save -exetex zref-test-abspos
+l3build save -eluatex zref-test-base
+l3build save -epdftex zref-test-base
+l3build save -exetex zref-test-base
+l3build save -eluatex zref-test-pagelayout
+l3build save -epdftex zref-test-pagelayout
+l3build save -exetex zref-test-pagelayout
+l3build save -eluatex zref-test-perpage-amstext
+l3build save -exetex zref-test-perpage-amstext
+l3build save -eluatex zref-test-perpage
+l3build save -epdftex zref-test-perpage
+l3build save -exetex zref-test-perpage
+l3build save -eluatex zref-test-titleref-memoir
+l3build save -epdftex zref-test-titleref-memoir
+l3build save -exetex zref-test-titleref-memoir
